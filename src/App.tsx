91c0ababd07a7bb5893796e3a85d92775f4026c9
Nathan Jones <njones@stridenyc.com>
2025-03-12 19:11:44 -0400

    Add PageNotFound component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import WorkoutListPage from './pages/WorkoutListPage';
-import AddWorkoutPage from './pages/AddWorkoutPage';
-import EditWorkoutPage from './pages/EditWorkoutPage';
-import WorkoutShowPage from './pages/WorkoutShowPage';
-import UserListPage from './pages/UserListPage';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import WorkoutListPage from "./pages/WorkoutListPage";
+import AddWorkoutPage from "./pages/AddWorkoutPage";
+import EditWorkoutPage from "./pages/EditWorkoutPage";
+import WorkoutShowPage from "./pages/WorkoutShowPage";
+import UserListPage from "./pages/UserListPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
             <Route path="/edit/:id" element={<EditWorkoutPage />} />
             <Route path="/workout/:id" element={<WorkoutShowPage />} />
             <Route path="/users" element={<UserListPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </main>
       </div>
