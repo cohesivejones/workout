71e62a12eeca0eb4ba434bd9e8f18998b2c1b610
Nathan Jones <njones@stride.build>
2025-05-03 17:03:30 -0400

    Use CORS env

--- server/src/index.ts
+++ server/src/index.ts
@@ -48,7 +48,7 @@ const app = express();
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    origin: (process.env.CORS_ORIGIN || "http://localhost:3000").split(','),
     credentials: true,
   })
 );
