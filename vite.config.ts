9c135df0288525f04eb5c66378d404acf71f20dc
Nathan Jones <njones@stride.build>
2025-10-12 09:21:30 -0400

    Use prod setup for CI e2e

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,17 @@
-import { defineConfig, loadEnv } from 'vite';
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import EnvironmentPlugin from 'vite-plugin-environment';
 import path from 'path';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react(), EnvironmentPlugin('all')],
-    server: {
-      port: 3000,
-      host: true, // Allow external connections
-      allowedHosts: ['turtle', 'localhost', '127.0.0.1'], // Allow specific hosts
-      proxy: {
-        '/api': {
-          target: env.VITE_API_URL,
-          changeOrigin: true,
-        },
-      },
-    },
+export default defineConfig({
+  plugins: [react(), EnvironmentPlugin('all')],
+  server: {
+    port: 3000,
+    host: true, // Allow external connections
+    allowedHosts: ['turtle', 'localhost', '127.0.0.1'], // Allow specific hosts
+  },
     resolve: {
       alias: {
         '@': path.resolve(__dirname, './src'),
@@ -57,5 +50,4 @@ export default defineConfig(({ mode }) => {
         reporter: ['text', 'json', 'html'],
       },
     },
-  };
 });
