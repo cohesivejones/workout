2cead74a9c6c105aa35361c4af7d20563469c94d
Nathan Jones <njones@stride.build>
2025-10-13 06:23:05 -0400

    Dont run playwright in parallel

--- playwright.config.ts
+++ playwright.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   testDir: './e2e/tests',
 
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
 
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
